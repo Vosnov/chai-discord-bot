7f5ae0b387b1262b9400736b2039644acabcc5f2
Vitaliy Stepanov <vstepanov@sfxdx.com>
2022-03-01 01:46:05 +0300

    dir fix

--- src/models/cache.ts
+++ src/models/cache.ts
@@ -7,4 +7,4 @@ export type Cache = {
 
 const root = path.resolve(__dirname, '..');
 export const cacheFile = `${root}/cache/cahce.json`
-export const cacheDir = `${root}`
+export const cacheDir = `${root}/cache`
